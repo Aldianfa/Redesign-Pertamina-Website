eae182a0c48faa8bd5ad5820c30c304e05661527
Aldian2306 <230605210010@student.uin-malang.ac.id>
2026-04-25 20:19:57 +0700

    fix: correct stylesheet path in bootstrap.blade.php for proper loading

--- resources/views/bootstrap.blade.php
+++ resources/views/bootstrap.blade.php
@@ -18,7 +18,7 @@
         crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/css/stylee.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 
     <!-- BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
