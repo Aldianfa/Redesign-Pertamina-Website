5632fc660b6d5d2cbc7f518b1e9f3fbe05f0800f
Aldian2306 <230605210010@student.uin-malang.ac.id>
2026-04-25 20:06:03 +0700

    fix: create framework views directory and update view compiled path

--- api/index.php
+++ api/index.php
@@ -6,8 +6,13 @@ if (!is_dir('/tmp/bootstrap-cache')) {
     mkdir('/tmp/bootstrap-cache', 0777, true);
 }
 
+if (!is_dir('/tmp/framework/views')) {
+    mkdir('/tmp/framework/views', 0777, true);
+}
+
 putenv('APP_CONFIG_CACHE=/tmp/bootstrap-cache/config.php');
 putenv('APP_SERVICES_CACHE=/tmp/bootstrap-cache/services.php');
 putenv('APP_PACKAGES_CACHE=/tmp/bootstrap-cache/packages.php');
+putenv('VIEW_COMPILED_PATH=/tmp/framework/views');
 
 require __DIR__.'/../public/index.php';
