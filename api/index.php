303b119406f4b9efadd1f4887abfe6643f986842
Aldian2306 <230605210010@student.uin-malang.ac.id>
2026-04-25 19:56:21 +0700

    fix: create bootstrap cache directory and update environment variables in Vercel configuration

--- api/index.php
+++ api/index.php
@@ -2,4 +2,12 @@
 
 $_SERVER['SCRIPT_NAME'] = '/index.php';
 
+if (!is_dir('/tmp/bootstrap-cache')) {
+    mkdir('/tmp/bootstrap-cache', 0777, true);
+}
+
+putenv('APP_CONFIG_CACHE=/tmp/bootstrap-cache/config.php');
+putenv('APP_SERVICES_CACHE=/tmp/bootstrap-cache/services.php');
+putenv('APP_PACKAGES_CACHE=/tmp/bootstrap-cache/packages.php');
+
 require __DIR__.'/../public/index.php';
